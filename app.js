c6007b04241a0dac1aa1facb700adfc766344fb4
Ehren Murdick <ehren@theedgecase.com>
2009-10-22 10:03:04 -0400

    player movement into a loop

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 $(function() {
   Game.init();
+  Game.start();
 });
--- game.js
+++ game.js
@@ -9,6 +9,24 @@ Game = {
       DOWN: 40
     }
 
+    Game.keyBindingsUp = function(e) {
+      console.log('keyup');
+      switch(e.which) {
+      case Game.keys.LEFT:
+        Game.player.stopX();
+        return false;
+      case Game.keys.RIGHT:
+        Game.player.stopX();
+        return false;
+      case Game.keys.UP:
+        Game.player.stopY();
+        return false;
+      case Game.keys.DOWN:
+        Game.player.stopY();
+        return false;
+      }
+    }
+
     Game.keyBindings = function(e) {
       switch(e.which) {
       case Game.keys.LEFT:
@@ -28,10 +46,14 @@ Game = {
 
     Game.start = function() {
       $(document).keydown(Game.keyBindings);
+      $(document).keyup(Game.keyBindingsUp);
+      Game.player.startLoop();
     }
 
     Game.stop = function() {
       $(document).unbind('keydown', Game.keyBindings);
+      $(document).unbind('keyup', Game.keyBindingsUp);
+      Game.player.stopLoop();
     }
   }
 }
