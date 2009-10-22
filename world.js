3b7d6a2bea6118cc67db9591fd3fe7f35bdaa1a9
Jon Distad <jon@theedgecase.com>
2009-10-22 09:55:32 -0400

    fixed moving off right and bottom of screen

--- world.js
+++ world.js
@@ -3,22 +3,30 @@ function World() {
   this._height = 400;
   this._tile = Raphael('game_window', this._width, this._height);
 
-  this.grantAvatar = function(x, y, radius) {
-    return this._tile.circle(x, y, radius);
-  }
-
-  this.monitor = function(event, callback) {
-    $(this._tile.canvas).bind(event, callback);
-  }
-
-  this.monitor('player:move', function(event, player) {
-    if (player.left() < 0)
-      player.moveRight();
-    if (player.right() > this._width)
-      player.moveLeft();
-    if (player.top() < 0)
-      player.moveDown();
-    if (player.bottom() > this._height)
-      player.moveUp();
-  })
+  this.monitorSignals();
 }
+
+$.extend(World.prototype, {
+  grantAvatar: function(x, y, radius) {
+    return this._tile.circle(x, y, radius);
+  },
+
+  monitor: function(event, callback) {
+    $(this._tile.canvas).bind(event, callback);
+  },
+  
+  monitorSignals: function() {
+    var self = this;
+    this.monitor('player:move', function(event, player) {
+      console.log(player.left(), player.right() + '>' + self._width, player.top(), player.bottom() + '>' + self._height);
+      if (player.left() < 0)
+        player.moveRight();
+      if (player.right() > self._width)
+        player.moveLeft();
+      if (player.top() < 0)
+        player.moveDown();
+      if (player.bottom() > self._height)
+        player.moveUp();
+    })
+  }
+})
